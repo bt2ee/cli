85a827cdf6052a445f7fb41247c39914d77ee6f8
chenle <chenle@forchange.tech>
2022-04-10 21:03:25 +0800

    feat: add rollup

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import babel from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
+
+export default {
+  banner: '#!/usr/bin/env node',
+  input: './bin/index.js',
+  plugins: [
+    replace({
+      delimiters: ['', ''],
+      '#!/usr/bin/env node': ''
+    }),
+    babel({
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: { node: '8' },
+            modules: false,
+            loose: true
+          }
+        ]
+      ]
+    }),
+    terser({
+      output: { comments: false }
+    }),
+    process.env.NODE_ENV === 'production' && uglify()
+  ],
+  output: [
+    {
+      file: 'lib/index.js',
+      format: 'cjs',
+      sourcemap: true
+    }
+  ]
+}
